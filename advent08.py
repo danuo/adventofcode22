e13e506f353d160bdb657e57979ee4057b9041c5
Daniel <d.brummerloh@gmail.com>
2022-12-09 00:12:10 +0100

    simplify code

--- advent08.py
+++ advent08.py
@@ -16,52 +16,55 @@ matrix_visible_trees[1:-1, 1:-1] = 0
 
 def c1_check_vis_top_to_down():
     for x in range(1, matrix_tree_heights.shape[1]-1):
-        ranges = (1, matrix_tree_heights.shape[0], 1), (matrix_tree_heights.shape[0]-2, 0, -1)
-        for ran in ranges:
-            level = matrix_tree_heights[ran[0]-ran[2], x]
-            for y in range(*ran):
-                if matrix_tree_heights[y, x] > level:
-                    matrix_visible_trees[y, x] = 1
-                level = max(matrix_tree_heights[y, x], level)
+        level = matrix_tree_heights[0, x]
+        for y in range(1, matrix_tree_heights.shape[0]):
+            if matrix_tree_heights[y, x] > level:
+                matrix_visible_trees[y, x] = 1
+            level = max(matrix_tree_heights[y, x], level)
 
-for _ in range(2):
-    c1_check_vis_top_to_down()
-    matrix_tree_heights = matrix_tree_heights.transpose()
-    matrix_visible_trees = matrix_visible_trees.transpose()
+c1_check_vis_top_to_down()
+matrix_tree_heights = np.flip(matrix_tree_heights, axis=0)
+matrix_visible_trees = np.flip(matrix_visible_trees, axis=0)
+c1_check_vis_top_to_down()
+matrix_tree_heights = matrix_tree_heights.transpose()
+matrix_visible_trees = matrix_visible_trees.transpose()
+c1_check_vis_top_to_down()
+matrix_tree_heights = np.flip(matrix_tree_heights, axis=0)
+matrix_visible_trees = np.flip(matrix_visible_trees, axis=0)
+c1_check_vis_top_to_down()
 
 print(np.count_nonzero(matrix_visible_trees))
 
 # ─── Challenge 2 ──────────────────────────────────────────────────────────────
 
-def c2_count_trees_from_y_x(y, x, matrix):
+def c2_count_trees_for_y_x(y, x, matrix):
     def find_trees_top_down(y, x, matrix):
-        score = 1
-        ranges = (y-1, -1, -1), (y+1, matrix.shape[1], 1)
-        for ran in ranges:
-            level = 0
-            counter = 0
-            for y_i in range(*ran):
-                height = matrix[y_i,x]
-                if height >= matrix[y,x]:
-                    counter += 1
-                    break
-                else:
-                    counter += 1
-                    if height >= level:
-                        level = height
-            score *= counter
-        return score
+        level = 0
+        tree_counter = 0
+        for y_i in range(y+1, matrix.shape[1]):
+            height = matrix[y_i,x]
+            if height >= matrix[y,x]:
+                tree_counter += 1
+                break
+            else:
+                tree_counter += 1
+                if height >= level:
+                    level = height
+        return tree_counter
 
     score = 1
-    for _ in range(2):
-        score *= find_trees_top_down(y, x, matrix)
-        matrix = matrix.transpose()
-        x, y = y, x
+    score *= find_trees_top_down(y, x, matrix)
+    matrix = np.flip(matrix, axis=0)
+    score *= find_trees_top_down(matrix.shape[0]-y-1, x, matrix)
+    matrix = matrix.transpose()
+    score *= find_trees_top_down(x, matrix.shape[0]-y-1, matrix)
+    matrix = np.flip(matrix, axis=0)
+    score *= find_trees_top_down(matrix.shape[1]-x-1, matrix.shape[0]-y-1, matrix)
     return score
 
 max_score = 0
 for y, x in np.ndindex(matrix_tree_heights.shape):
-    score = c2_count_trees_from_y_x(y, x, matrix_tree_heights)
+    score = c2_count_trees_for_y_x(y, x, matrix_tree_heights)
     if score > max_score:
         max_score = score
 print(max_score)
